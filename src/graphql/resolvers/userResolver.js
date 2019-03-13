ebef49a5ad7652ccd4f601241b7d5a37bd3ad04a
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-03-13 00:15:55 +0000

    Made userResolver functions significantly lighter on code, and much easier to understand. Need to test them

--- src/graphql/resolvers/userResolver.js
+++ src/graphql/resolvers/userResolver.js
@@ -1,56 +1,25 @@
 export default {
   Query: {
-
     getUser: async(parent, args, context, info) => {
-      let response;
       const { User } = context.mongooseModels;
-      await User
+      return await User
         .findById(args.id, 'id username email')
-        .then((err, user) => {
-          if (err) {
-            console.log(err);
-            throw new Error("Mongoose findOne() error during getUser Query");
-          }
-          else {
-            response = {
-              id: user.id,
-              username: user.username,
-              email: user.email
-            };
-          }
-        });
-      return response;
+        .catch(err => console.log(err));
     },
-
     allUsers: async(parent, args, context, info) => {
-      let response;
       const { User } = context.mongooseModels;
-      await User
+      return await User
         .find({}, 'id username email')
-        .then((err, users) => {
-          if (err) {
-            console.log(err);
-            throw new Error("Mongoose find() error during allUsers Query");
-          }
-          else {
-            response = users;
-          }
-        });
-      return response;
+        .catch(err => console.log(err));
     }
-
   },
-
   Mutation: {
     createUser: async(parent, args, context, info) => {
       const { User } = context.mongooseModels;
       const newUser = new User(args);
-      const { id, username, email } = await newUser.save().then((err, user) => {
-        if (err) {
-          console.log(err);
-          throw new Error("Mongoose save() error during createUser Mutation");
-        }
-      });
+      const { id, username, email } = await newUser
+        .save()
+        .catch(err => console.log(err));
       return { id, username, email };
     }
   }
